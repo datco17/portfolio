c25f8b25ba2f328631f19695e958d8de4f88c515
baitapcuatoi <nhoxpro1704@gmail.com>
2025-05-23 03:18:39 +0700

    add index.html

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.{html,js}"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
